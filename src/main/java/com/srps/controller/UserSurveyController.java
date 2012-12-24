923e9045aee40dca3de2a6147f2a0f60a8f20ebf
dev-group <rafiullahrs@gmail.com>
2012-12-24 04:59:14 +0000

    mobile auth config - rafi

--- src/main/java/com/srps/controller/UserSurveyController.java
+++ src/main/java/com/srps/controller/UserSurveyController.java
@@ -1,18 +1,36 @@
 package com.srps.controller;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.srps.util.FormUtil;
 
 @Controller
 public class UserSurveyController {
 	
-	@RequestMapping(value="/homess")
+	@RequestMapping(value="/")
 	public ModelAndView home() { 
 		
 		ModelAndView mav = new ModelAndView(); 
 		mav.setViewName("index"); 
 		
 		return mav; 
+	}
+	
+	@RequestMapping(value="/mobile/formList")
+	public void getFormList(HttpServletResponse response) throws IOException { 
+		
+		String formList = FormUtil.prepareFormList(null); 
+		
+		response.setStatus(200); 
+		response.setContentType("text/xml"); 
+		response.getOutputStream().print(formList); 
+		response.flushBuffer(); 
 	}
 }

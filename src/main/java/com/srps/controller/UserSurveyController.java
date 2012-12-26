27594e9b1ad36488524f7936ae4cf75739aba343
dev-group <rafiullahrs@gmail.com>
2012-12-26 23:19:35 +0000

    working multiple file upload - raf

--- src/main/java/com/srps/controller/UserSurveyController.java
+++ src/main/java/com/srps/controller/UserSurveyController.java
@@ -1,13 +1,16 @@
 package com.srps.controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.srps.util.FormUtil;
@@ -44,7 +47,8 @@ public class UserSurveyController {
 	}
 	
 	@RequestMapping(value="/upload", method=RequestMethod.POST)
-	public void upload(MultipartResolver[] files) { 
-		System.out.println("files.length = " + files.length); 
+	public void upload(MultipartHttpServletRequest request) {
+		System.out.println("test");
+		System.out.println("files.size = " + request.getMultiFileMap().size()); 
 	}
 }
--- src/main/webapp/WEB-INF/view/upload.jsp
+++ src/main/webapp/WEB-INF/view/upload.jsp
@@ -5,7 +5,8 @@
 	<body>
 		<h1>Please upload a file</h1>
 		<form method="post" action="http://localhost:8080/SRPS/upload" enctype="multipart/form-data">
-			<input type="file" name="file" />
+			<input type="file" name="multipartFile" />
+			<input type="file" name="multipartFile2" />
 			<input type="submit" name="upload"/>
 		</form>
 	</body>

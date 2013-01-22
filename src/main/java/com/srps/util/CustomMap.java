5e0fec5ca1f6b6c16b5a389693f729e149b54ace
dev-group <rafiullahrs@gmail.com>
2013-01-22 01:00:25 +0000

    a model class with same structure as map to ease jdbc query - raf

---
+++ src/main/java/com/srps/util/CustomMap.java
@@ -0,0 +1,24 @@
+package com.srps.util;
+
+
+/*
+ * This is used to generate a key/value map of form_id and form_name
+ */
+
+public class CustomMap {
+	private Object key; 
+	private Object value;
+	
+	public Object getKey() {
+		return key;
+	}
+	public void setKey(Object key) {
+		this.key = key;
+	}
+	public Object getValue() {
+		return value;
+	}
+	public void setValue(Object value) {
+		this.value = value;
+	} 
+}
